94ba71e1d049b07171dd198d6a824363d4524fbc
rweastman-lgtm <rweastman@aol.com>
2025-09-14 12:21:23 -0500

    Update fmv_analyzer_app.py

--- fmv_analyzer_app.py
+++ fmv_analyzer_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import datetime
 import re
 
-from zip_risk_lookup import zip_to_risk  # ← Add this line here
+from zip_risk_lookup import zip_to_risk
 
 def extract_zip(address):
     match = re.search(r"\b\d{5}\b", address)
