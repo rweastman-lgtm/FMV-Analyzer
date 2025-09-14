04727f2e7190f4805efc2718f1d86d72de92ca48
rweastman-lgtm <rweastman@aol.com>
2025-09-14 11:53:51 -0500

    Update zip_risk_lookup.py

--- zip_risk_lookup.py
+++ zip_risk_lookup.py
@@ -8,12 +8,19 @@ def load_risk_tables():
 
 def zip_to_risk(zip_code):
     flood_df, wind_df, fire_df = load_risk_tables()
-    flood_zone = flood_df.loc[flood_df["ZIP"] == zip_code, "FloodZone"].values[0]
-    wind_zone = wind_df.loc[wind_df["ZIP"] == zip_code, "WindZone"].values[0]
-    fire_score = fire_df.loc[fire_df["ZIP"] == zip_code, "FireRiskScore"].values[0]
+
+    flood_match = flood_df.loc[flood_df["ZIP"] == zip_code, "FloodZone"]
+    wind_match = wind_df.loc[wind_df["ZIP"] == zip_code, "WindZone"]
+    fire_match = fire_df.loc[fire_df["ZIP"] == zip_code, "FireRiskScore"]
+
+    flood_zone = flood_match.values[0] if not flood_match.empty else "X"
+    wind_zone = wind_match.values[0] if not wind_match.empty else "Zone III"
+    fire_score = fire_match.values[0] if not fire_match.empty else 3
+
     return {
         "flood_zone": flood_zone,
         "wind_zone": wind_zone,
         "fire_risk_score": fire_score
     }
+
 
